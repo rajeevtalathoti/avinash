0746a76c46f58547891a8f064aa2a50366fd6ca9
avinash <rajeevtalathoti@gail.com>
2018-10-02 21:27:15 -0700

    updated0.01

---
+++ first.py
@@ -0,0 +1,22 @@
+a=10;
+print(a)
+print(type(a))
+print(id(a))
+
+y="true"
+print(y)
+print(type(y))
+print(id(y))
+
+x=5.9574
+print(x)
+print(type(x))
+print(id(x))
+
+
+print('sum is :' , a+x)
+print("sub is :" , a-x)
+print("mul is :" , a*x)
+print("div is :" , a/x)
+print("Mod is :" , a%x)
+print("complete")
